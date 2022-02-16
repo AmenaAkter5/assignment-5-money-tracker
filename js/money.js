f3d586763d7d267766b138867e623565ea4f4599
AmenaAkter <latapata1952@gmail.com>
2022-02-17 04:01:01 +0600

    js-code-added

--- js/money.js
+++ js/money.js
@@ -1,48 +1,79 @@
 
+
+// function - 1: To get input value of every input field
+
 function getInputValue(inputId) {
-    // input field
+    // access input field
     const inputField = document.getElementById(inputId);
     const inputValue = inputField.value;
     const newInputAmount = parseInt(inputValue);
-
-    // clear the input field
-    // inputField.value = '';
-
     return newInputAmount;
 }
 
 
-// function - 1: Add all Expenses
+// function - 2: Add all Expenses
 
 function totalExpenses() {
-    // get Food Cost Value
+    // expense sectors
     const foodCost = getInputValue('food-cost');
-
-    // get Rent Cost Value
     const rentCost = getInputValue('rent-cost');
-
-    // get Cloth Cost Value
     const clothCost = getInputValue('cloth-cost');
 
-    // total Expenses
+    // total expense
     const totalExpense = foodCost + rentCost + clothCost;
 
+    // update total expense field
     const expenseField = document.getElementById('expense-field');
+
     expenseField.innerText = totalExpense;
     return totalExpense;
+}
+
+
+// function - 3: Savings Part
+
+function savings() {
+    const currentIncome = getInputValue('income-input');
+    const savingsInput = getInputValue('save-input');
+
+    // get savings amount
+    const savings = (currentIncome * savingsInput) / 100;
+
+    // update savings amount field
+    const saveField = document.getElementById('save-amount');
+    saveField.innerText = savings;
+}
+
+// function - 4: update balance and remaining-balance amount
+
+function updateBalance() {
+    const currentIncome = getInputValue('income-input');
+    const expenseTotal = totalExpenses();
+
+    // Balance
+    const totalBalance = currentIncome - expenseTotal;
+
+    // update balance amount
+    const balanceField = document.getElementById('total-balance');
+    balanceField.innerText = totalBalance;
+
+    savings();
+    // remaining balance
+    // const remainingBalance = 
+    const remainingField = document.getElementById('remaining-balance');
 }
 
 
 // calculate button handler
 
 document.getElementById('calculate-button').addEventListener('click', function () {
-    // get income value
-    const currentIncome = getInputValue('income-input');
+    updateBalance();
+})
 
-    // get totalExpense
-    const expenseTotal = totalExpenses();
 
-    // Balance
-    const netBalance = currentIncome - expenseTotal;
-    console.log(netBalance);
+
+// save button handler
+
+document.getElementById('save-button').addEventListener('click', function () {
+    updateBalance();
 })
